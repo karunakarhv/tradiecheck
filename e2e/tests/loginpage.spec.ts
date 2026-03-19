ebcfd62b897d4f6c973c07bf5d856bc85920d1bd
karunakarhv <karunakarhv@gmail.com>
2026-03-20 01:28:24 +1100

    Fix test, some more modification #2

--- e2e/tests/loginpage.spec.ts
+++ e2e/tests/loginpage.spec.ts
@@ -1,10 +1,15 @@
 import { test, expect } from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
+import * as dotenv from "dotenv"; // or import dotenv from 'dotenv';
+dotenv.config();
 
 test.describe("Login Features", async () => {
   test("allows user to login with valid credentials", async ({ page }) => {
     const loginPage = new LoginPage(page);
-    await loginPage.login("test@test.com", "test$123");
+    await loginPage.login(
+      process.env.TEST_EMAIL || "",
+      process.env.TEST_PASSWORD || "",
+    );
     await expect(page).toHaveURL("/welcome");
 
     await loginPage.logout();
@@ -15,7 +20,10 @@ test.describe("Login Features", async () => {
     page,
   }) => {
     const loginPage = new LoginPage(page);
-    await loginPage.login("test@test1.com", "test$123");
+    await loginPage.login(
+      process.env.INVALID_EMAIL || "",
+      process.env.INVALID_PASSWORD || "",
+    );
 
     await expect(
       page.getByText("Invalid login credentials", {
